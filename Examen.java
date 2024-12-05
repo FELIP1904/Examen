7974d8be88a2e5983aa0f97c4a2835cf17b78825
felipe <juanfe1234gmail.com>
2024-12-05 16:26:08 +0100

    contador_de_letras

--- Examen.java
+++ Examen.java
@@ -34,6 +34,16 @@ public class Examen {
                 }
 
                 case 3 -> {
+                    System.out.print("Ingrese una frase: ");
+                	String fraseParaContar = scanner.nextLine();
+                	System.out.print("Ingrese la letra a buscar: ");
+                	char letra = scanner.nextLine().charAt(0);
+                	int contador = 0;
+                	for (char c : fraseParaContar.toCharArray()) {
+                    	if (c == letra) {
+                        	contador++;
+                    	}
+                	}
                   
                 }
 
